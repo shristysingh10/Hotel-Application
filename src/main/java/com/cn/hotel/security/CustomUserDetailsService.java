6828ad4fdd88bc4d8438c4c72fa0f1e137ade02c
Shristy Singh <112796719+shristysingh10@users.noreply.github.com>
2024-06-18 10:14:17 +0530

    Add files via upload

---
+++ src/main/java/com/cn/hotel/security/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.cn.hotel.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.cn.hotel.model.User;
+import com.cn.hotel.repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+	private UserRepository userRepository;
+	
+	public CustomUserDetailsService(UserRepository userRepository)
+	{
+		this.userRepository = userRepository;
+	}
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		User user= this.userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User Not Found"));
+		return user;
+		
+	}
+
+	
+}
